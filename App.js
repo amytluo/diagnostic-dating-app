ad9385c30573b2a88fd0c3ce3b43aa6ce854dea9
amxh <amy.luo42@gmail.com>
2020-01-26 07:45:51 -0500

    fixed imports and semantics

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { home } from './components/home';
-import { swipe } from './components/swipe.js';
-import { match } from './components/match.js';
+import home from './components/home';
+import swipe from './components/swipe.js';
+//import match from './components/match.js';
 
 
 const MainNavigator = createStackNavigator(
   {
     Home: { screen: home },
     Swipe: { screen: swipe },
-    Match: { screen: match }
+    //Match: { screen: match }
   },
   {
     initialRouteName: 'Home',
