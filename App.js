9ad26fbfa5aaa98f19e49749ef9ed07ed8443e6d
FlippadyFlap <46036342+FlippadyFlap@users.noreply.github.com>
2020-01-26 01:07:35 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { home, swipe, match } from 'Components';
 
-const MainNavigator = createStackNavigator({
-  Home: {screen: HomeScreen},
-  Profile: {screen: ProfileScreen},
-});
+
+const MainNavigator = createStackNavigator(
+  {
+    Home: { screen: home },
+    Swipe: { screen: swipe },
+    Match: { screen: match }
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
 
 const App = createAppContainer(MainNavigator);
 
